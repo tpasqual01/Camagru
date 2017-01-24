621b85260248dc955d5765ae1a571d0fbab758b5
dlievre <42@photeam.com>
2017-01-24 19:44:25 +0100

    Add files via upload

--- header.php
+++ header.php
@@ -1,1 +1,7 @@
-<div id="header"><h1>Camagru</h1></div>
+<?php
+if ($_SESSION["valide"]=='ok') {
+	print '<div id="header"><h1>Camagru</h1>'.$_SESSION["user"].' logoff</div>';
+}
+else
+	print '<div id="header"><h1>Camagru</h1>'.'</div>';
+?>
